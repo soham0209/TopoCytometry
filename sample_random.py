69c73e15e78571c4db7b4012b46a046e046c48cd
soham0209 <soham.juetce@gmail.com>
2021-07-13 16:34:55 +0530

    Edited random sampling

--- sample_random.py
+++ sample_random.py
@@ -1,43 +1,35 @@
 import numpy as np
-import os
-from seed_pairs import *
 import sys
+import os
+import argparse
+import pandas as pd
+
+
+def sample(x, outfile):
+    num_samples = args.num_kde_samples
+    sample_indices = np.random.randint(0, x.shape[0], num_samples)
+    print('Drawing samples')
+    x_hat = x[sample_indices[0:num_samples]]
+    kde_samples = outfile + '_fvert.txt'
+    np.savetxt(kde_samples, x_hat, fmt='%.4f')
+
 
 if __name__ == '__main__':
-    dir_name = sys.argv[1]
-    # dir_name = file_name[0:-4]
-    num_samples = 20000
-    if len(sys.argv) > 2:
-        num_samples = int(float(sys.argv[2]))
-    
-    cohort_type = 'Healthy_data'
-    if 'P' in dir_name:
-        cohort_type = 'Patient_data'
-    elif 'R' in dir_name:
-        cohort_type = 'Recovered_data'
-    X = np.loadtxt(os.path.join('Data', cohort_type, dir_name + '.csv'), delimiter=',')
-    # protein_indices = [10, 17, 22]
-    protein_indices = [10, 17, 22]
-    print('Loaded ', dir_name + '.csv')
-    X = X[:, protein_indices]
-    print('Drawing random samples')
-    # num_samples = min(num_samples, X.shape[0])
-    sample_indices = np.random.randint(0, X.shape[0], num_samples)
-    """
-    For now take the first num_samples. Change it later.
-    """
-    print('Drawing samples')
-    X_hat = X[sample_indices[0:num_samples]]
-    root_dir = 'recovered_data'
-    if not os.path.exists(root_dir):
-        os.mkdir(root_dir)
-    tmp_dir_name = os.path.join(root_dir, dir_name)
-    if not os.path.exists(tmp_dir_name):
-        os.mkdir(tmp_dir_name)
-    outfile = tmp_dir_name + '/' + dir_name + '_fvert.txt'
-    print('Saving samples', outfile)
-    f = open(outfile, 'w')
-    f.write(str(3) + '\n')
-    np.savetxt(f, X_hat, fmt='%.4f')
-    f.close()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', type=str, required=True, help='Filename to perform kde')
+    parser.add_argument('-p', '--proteins', type=int, nargs='+', default=[10, 17, 22], help='Proteins to consider')
+    parser.add_argument('-n', '--num_kde_samples', type=int, default=20000, help='Sample from KDE')
 
+    args = parser.parse_args()
+    file_name = 'data/' + args.filename
+    if '.csv' in file_name:
+        file_name = file_name[-4:]
+    dir_name = 'KDE_samples/'
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    protein_indices = args.proteins
+    X = pd.read_csv(file_name + '.csv', sep=',').to_numpy()
+    print('Loaded ', file_name + '.csv')
+    X = X[:, protein_indices]
+    outfilename = os.path.join(dir_name, args.filename)
+    sample(X, outfilename)
